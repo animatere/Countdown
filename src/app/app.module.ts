542aa417704f57be7f970c5206e9254b4f22489b
animatere <animatere@gmail.com>
2020-07-22 19:30:54 +0200

    test commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { TestComponent } from './test/test.component';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {TestComponent} from './test/test.component';
+import {GitTestComponent} from './git-test/git-test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    GitTestComponent
   ],
   imports: [
     BrowserModule,
